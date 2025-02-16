c497784606ac2ef451d1c2b9094cb39713dc0f22
Sultan Mukhtarov <suli419956@gmail.com>
2025-02-16 15:37:17 +0600

    checked token.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import { User, UserDocument } from '../schemas/user.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { RegisterUserDto } from './register-user.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { TokenAuthGuard } from '../auth/token-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -25,5 +26,12 @@ export class UsersController {
   @Post('sessions')
   login(@Req() req: Request) {
     return req.user;
+  }
+
+  @UseGuards(TokenAuthGuard)
+  @Get('secret')
+  secret(@Req() req: Request) {
+    const user = req.user as UserDocument;
+    return { message: 'Secret message', email: user.email };
   }
 }
